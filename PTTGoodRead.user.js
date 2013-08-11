56779aab13c4722a750917e8deb9f1029ecc767d
s25g5d4 <s25g5d4@gmail.com>
2013-08-11 23:48:02 +0800

    change some variable name and add comment.

--- PTTGoodRead.user.js
+++ PTTGoodRead.user.js
@@ -2,42 +2,45 @@
 // @name        PTTGoodRead
 // @namespace   http://www.ptt.cc/bbs/*
 // @include     http://www.ptt.cc/bbs/*
-// @version     0.1 alpha
+// @version     0.1.1
 // @grant       none
 // ==/UserScript==
 
 // custom variable
+// http://www.csscompressor.com/
 var insertCSS = '#overlay{position:fixed;width:100%;height:100%;background:rgba(0,0,0,0.8);z-index:-999}#overlay.activeLightbox{z-index:999}#mainContainer{width:100%}#mainContainer.activeLightbox{position:fixed}.lightbox{text-decoration:none;border:none;background:none}.lightbox:hover{background:none}';
 
 // declare variable
-var a, matchedNode, showLightbox, PTTGoodRead = {}, addLightboxNode = [];
+var PTTGoodRead = {},
+    addLightboxNode = [],
+    toArray = function (obj) {
+      return Array.prototype.slice.call(obj, 0);
+    },
+    links,
+    matchedNode,
+    showLightbox;
 
-showLightbox = function () {
-  console.log(arguments);
+showLightbox = function (event) {
+  
 }
 
-PTTGoodRead.init = function () {
-  console.log(window.$);
-}
-
-a = document.querySelectorAll('#mainContent a');
 // Convert array-like object into native array. For performance and additional method.
-a = Array.prototype.slice.call(a, 0);
+links = toArray( document.querySelectorAll('#mainContent a') );
 
 // find out image links
-matchedNode = a.filter(function (e) {
-  var nextElement = e.nextSibling;
+matchedNode = links.filter(function (element) {
+  var nextElement = element.nextSibling;
   while (nextElement.nodeType !== Node.ELEMENT_NODE) nextElement = nextElement.nextSibling;
   if (nextElement.className.match('richcontent')) {
     addLightboxNode.push(nextElement);
     return false;
   }
-  return e.href.match(/(jpg|png|gif|bmp|imgur.*)$/i);
+  return element.href.match(/(jpg|png|gif|bmp|imgur.*)$/i);
 });
 
 // generate <img> and <a> for image links.
-matchedNode.forEach(function (e) {
-  var url = e.href;
+matchedNode.forEach(function (element) {
+  var url = element.href;
   var insertHTML = '<div class="richcontent">' +
                      '<a href="' + url + '" alt="image link" target="_blank" rel="nofollow" class="lightbox">' +
                        '<img src="' + ( url.match(/\/imgur/i) ?
@@ -45,23 +48,25 @@ matchedNode.forEach(function (e) {
                        '" onerror="PTTGoodRead.handleErrorImg(event, this)">' +
                      '</a>' +
                    '</div>';
-  e.insertAdjacentHTML('afterend', insertHTML);
+  element.insertAdjacentHTML('afterend', insertHTML);
 });
 
 // add <a> to div.richcontent
-addLightboxNode.forEach(function (e) {
-  var imgSrc = e.innerHTML.match(/src="([^"]+)"/i)[1];
-  e.innerHTML = '<a href="' + imgSrc + '" alt="image link" target="_blank" rel="nofollow" class="lightbox">' +
-                  '<img src="' + imgSrc + '" onerror="PTTGoodRead.handleErrorImg(event, this)">' +
-                '</a>';
+addLightboxNode.forEach(function (element) {
+  var imgSrc = element.innerHTML.match(/src="([^"]+)"/i)[1];
+  element.innerHTML = '<a href="' + imgSrc + '" alt="image link" target="_blank" rel="nofollow" class="lightbox">' +
+                        '<img src="' + imgSrc + '" onerror="PTTGoodRead.handleErrorImg(event, this)">' +
+                      '</a>';
 });
 
+/*
 // insert a layer for lightbox
-document.body.insertAdjacentHTML('afterbegin', '<div id="overlay"><div id="lightbox"><img id="lightboxImg" src=""></div></div>');
-
+document.body.insertAdjacentHTML('afterbegin', '<div id="overlay"><div id="lightbox"></div></div>');
+*/
 // insert CSS rule before </body>
 document.body.insertAdjacentHTML('beforeend', '<style>' + insertCSS + '</style>');
-
+/*
 Array.prototype.slice.call(document.querySelectorAll('a.lightbox'), 0).forEach(function (e) {
   e.addEventListener('click', showLightbox);
 });
+*/
